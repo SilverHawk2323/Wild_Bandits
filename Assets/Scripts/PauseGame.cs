f258146853703174a2a43a0c77b9d9294d8c215e
JustSomeK <117633204+JustSomeK@users.noreply.github.com>
2024-08-30 13:01:16 +1000

    added pause menu
    pause menu and esc toggle

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class PauseGame : MonoBehaviour
+{
+    [SerializeField] GameObject PauseMenu;
+    public bool gamePaused;
+    [SerializeField] GameObject ResumeButton;
+    [SerializeField] GameObject ExitButton;
+    
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Escape) && gamePaused == false)
+       {
+            Paused();
+       }
+       else if (Input.GetKeyDown(KeyCode.Escape) && gamePaused == true)
+       {
+            Resume();
+       }
+
+       if (gamePaused == true)
+       {
+            Time.timeScale = 0;
+       }
+       else if (gamePaused == false)
+       {
+            Time.timeScale = 1;
+       }
+
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        gamePaused = false;
+    }
+    
+    public void Paused()
+    {
+        PauseMenu.SetActive(true);
+        gamePaused = true;
+    }
+}
